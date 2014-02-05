37bf651c05e879ad772f244ce60b16eee7cbb608
Zach Ferland <zachferland@gmail.com>
2014-02-05 13:25:43 -0500

    text purpose

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,29 +20,30 @@ $(function(){ $(document).foundation(); });
 
 
 $(document).ready(function() {  
-var stickyNavTop = $('form').offset().top - 20;  
-  
-var stickyNav = function(){  
-var scrollTop = $(window).scrollTop();  
-       
-if (scrollTop > stickyNavTop) {   
-    $('form').addClass('sticky');
-    $('.what').css({'margin-bottom': 110});
-} else {  
-    $('form').removeClass('sticky');
-    $('.what').css({'margin-bottom': 70});   
-}  
-};  
-  
-stickyNav();  
 
-$(window).scroll(function(i){
-    $('.title-top').css({'opacity':( 110-($(window).scrollTop()/2.7) )/100});
-    $('.what').css({'opacity':( 135-($(window).scrollTop()/2.7) )/100});
-    $('.join-others').css({'opacity':( 135-($(window).scrollTop()/2.7) )/100});
-    $('.down-arrow').css({'opacity':( 200-($(window).scrollTop()/2.7) )/100});
-    $('#header-fixed').css({'opacity':( ($(window).scrollTop()/1.8) - 280)/100});
-    $('#header-fixed').css({'height':( ($(window).scrollTop()/1.3) - 400)});
-    stickyNav();  
-});
+if ($(window).width() > 670) {
+	var stickyNavTop = $('form').offset().top - 20;  
+	var stickyNav = function(){  
+		var scrollTop = $(window).scrollTop();  
+   		if (scrollTop > stickyNavTop) {   
+    		$('form').addClass('sticky');
+    		$('.what').css({'margin-bottom': 110});
+		} else {  
+    		$('form').removeClass('sticky');
+    		$('.what').css({'margin-bottom': 70});   
+		}  
+	};  
+  
+	stickyNav();  
+
+	$(window).scroll(function(i){
+    	$('.title-top').css({'opacity':( 110-($(window).scrollTop()/2.7) )/100});
+    	$('.what').css({'opacity':( 135-($(window).scrollTop()/2.7) )/100});
+    	$('.join-others').css({'opacity':( 135-($(window).scrollTop()/2.7) )/100});
+    	$('.down-arrow').css({'opacity':( 200-($(window).scrollTop()/2.7) )/100});
+    	$('#header-fixed').css({'opacity':( ($(window).scrollTop()/1.8) - 280)/100});
+    	$('#header-fixed').css({'height':( ($(window).scrollTop()/1.3) - 400)});
+    	stickyNav();  
+	});
+}
 });  
