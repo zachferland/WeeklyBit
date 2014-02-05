f252aa78aa4b4b60c47118fb696be1882f11e6ad
Zach Ferland <zachferland@gmail.com>
2014-02-05 11:21:50 -0500

    design, mailchimp count

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby "2.0.0"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
@@ -9,10 +11,12 @@ gem 'mailchimp-api', require: 'mailchimp'
 
 gem 'thin'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'rails_12factor', group: :production
 
-# gem "pg"
+# Use sqlite3 as the database for Active Record
+# gem 'sqlite3'
+
+gem "pg"
 
 gem 'figaro'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,18 @@
 class PagesController < ApplicationController
   def index
+    @member_count = @mc.lists.list({'list_id' => '5179b9cf9c'})['data'][0]['stats']['member_count'] + 83
+     # = lists_res['data'][15].member_count
   end
 
   def subscribe
   	email = params['email']
     begin
       @mc.lists.subscribe('5179b9cf9c', {'email' => email})
-      flash[:success] = "#{email} subscribed successfully"
+      flash[:success] = "Thanks, #{email} subscribed successfully! Emails will be delivered every Wednesday, enjoy!"
     rescue Mailchimp::ListAlreadySubscribedError
-      flash[:error] = "#{email} is already subscribed to the list"
+      flash[:error] = "#{email} is already subscribed to the list."
     rescue Mailchimp::ListDoesNotExistError
-      flash[:error] = "The list could not be found"
+      flash[:error] = "The list could not be found."
       redirect_to root_path
       return
     rescue Mailchimp::Error => ex
